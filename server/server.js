7a3e7df0bb8d8b5e64e03c71f51325dd16760900
lawkumar9@gmail.com <ashishism10@gmail.com>
2018-10-02 21:57:07 +0530

    update server file

--- server/server.js
+++ server/server.js
@@ -4,13 +4,20 @@ mongoose.connect('mongodb://localhost:27017/MongooseApp');
 
 var todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim : true
+
   },
   completed : {
-    type: Boolean
+    type: Boolean,
+    default : false
+
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -24,14 +31,35 @@ var todo = mongoose.model('Todo', {
 //   console.log("unable to save todo");
 // });
 
-var otherTodo = new todo({
-  text : 'hey suvu',
-  completed : true,
-  completedAt : 234
+// var otherTodo = new todo({
+//   text : ' hey savu  ',
+//   //completed : true,
+//   //completedAt : 234
+// });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(doc);
+// }, error => {
+//   console.log("unable to save otherTodo");
+// });
+
+var User = mongoose.model('User', {
+
+  email : {
+    type : String,
+    minlength : 1,
+    trim: true,
+    required : true
+  }
+
 });
 
-otherTodo.save().then((doc) => {
+var user = new User ({
+  email: "  lawkumar9@gmail.com  "
+});
+
+user.save().then((doc) => {
   console.log(doc);
-}, error => {
-  console.log("unable to d=save otherTodo");
+}, e => {
+  console.log("unable to save todo");
 });
